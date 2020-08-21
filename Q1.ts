11e63578b7d99820e197296bc87d9a8661e5ecf4
博文 <zhaobowen@mockuai.com>
2020-08-21 13:48:48 +0800

    upd

--- Q1.ts
+++ Q1.ts
@@ -1,22 +1,2 @@
-type Lengthwise = {
-	length: number;
-};
-
-function createList<T extends number | Lengthwise>(): T[] {
-	return [] as T[];
-}
-
-const numberList = createList<number>(); // ok
-const stringList = createList<string>(); // ok
-const arrayList = createList<any[]>(); // ok
-const boolList = createList<boolean>(); // error
-
-// type NoReturnFn = () => void;
-// interface NoReturnFn {
-//   (...args: any[]): void;
-// }
-type NoReturnFn = (...args: any[]) => void;
-const foo: NoReturnFn = (n: number) => n;
-
-// function debounce<F extends NoReturnFn>(fn: F) {}
-// debounce(foo);
+const wtf: () => void = () => 1;
+const wtf2: () => undefined | void = () => 1;
--- Q2.ts
+++ Q2.ts
@@ -4,6 +4,7 @@ class Animal {
 
 class Bee extends Animal {}
 
+//? where is the document about `new`?
 function createInstance<A extends Animal>(c: new () => A): A {
 	return new c();
 }
